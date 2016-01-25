cbf7997a26c7d855fe7bf717ae0eb6f1ff144ccd
FrankOMearaHaslam <franciskildare21@live.co.uk>
2016-01-25 13:27:47 +0000

    Added sites code
    Updates the main python file

---
+++ data/raw_data.py
@@ -0,0 +1,10 @@
+
+flights = { '09:35': 'Freeport',
+            '17:00': 'Freeport',
+            '19:00': 'Freeport',
+            '09:55': 'West End',
+            '10:45': 'Treasure Cay',
+            '11:45': 'Rock Sound',
+            '17:55': 'Rock Sound',
+            '12:00': "Arthur's Town", }
+
